a44183312f5b5ce4254637cb3751b36ba99987f4
TheExplosion3 <69405764+TheExplosion3@users.noreply.github.com>
2024-04-26 21:05:24 -0400

    debugged, though if you insert a large number it WILL freeze eventually due to the amount of ram the structs eat

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,21 @@ use std::{i128};
 extern crate rand;
 use rand::{Rng};
 
-const NAMELIST: &'static [&'static str] = &["bitch", "cunt"];
-const MODIFIERLIST: &'static [&'static str] = &["firefox", "chrome"];
+const NAMELIST: &'static [&'static str] = &["guh?", "Cunt"];
+const MODIFIERLIST: &'static [&'static str] = &[" Greater ", " Inferior ", " Masterful "];
+const DESCRIPTIONLIST: &'static [&'static str] = &["Kills you instantly.", "Makes you serve.", "Transes your gender."];
+
 
 fn main() {
 
     println!("How many potions would you like to generate? Enter a singular number.");
-
     let mut num_pot: i128 = 0;
     let mut num_pot_string: String = String::new();
     loop {
 
         let mut breaker: bool = false;
         std::io::stdin().read_line(&mut num_pot_string).expect("failed to readline");
-        let parser = num_pot_string.parse::<i128>();
+        let parser = num_pot_string.trim().parse::<i128>();
 
         match parser {
             Ok(res) => {
@@ -24,6 +25,7 @@ fn main() {
             },
             Err(_err) => {
                 num_pot = -1;
+                num_pot_string = String::new();
             }
         };
 
@@ -38,20 +40,20 @@ fn main() {
             break;
         }
     }
-
+    println!();
     let mut pot_arr: Vec<Potion> = vec![];
     let mut num_randomizer = rand::thread_rng();
-    for i in 1..num_pot {
+    for i in 0..num_pot {
         pot_arr.push(Potion {
-            potion_number: i,
+            potion_number: i+1,
             name: randomizer("name"),
             strength_modifier: randomizer("modifier"),
-            description: randomizer("name"),
+            description: randomizer("description"),
             number_of_potions: num_randomizer.gen_range(0..i128::MAX) });
     }
 
     for i in pot_arr.into_iter() {
-        println!("{}.{}Potion of {}\n{}.\n You have {} of these potions.", i.potion_number, i.strength_modifier, i.name, i.description, i.number_of_potions);
+        println!("{}.{}Potion of {}\n\tDescription: {}\n\tYou have {} of these potions.", i.potion_number, i.strength_modifier, i.name, i.description, i.number_of_potions);
     }
 }
 
@@ -66,10 +68,13 @@ struct Potion {
 fn randomizer(s: &str) -> String {
     let mut rnd = rand::thread_rng();
     if s == "name" {
-        return NAMELIST.get(rnd.gen_range(1..NAMELIST.len())).expect("Const list, string should return.").to_string();
+        return NAMELIST.get(rnd.gen_range(0..NAMELIST.len())).expect("Const list, string should return.").to_string();
     }
     else if s == "modifier" {
-        return MODIFIERLIST.get(rnd.gen_range(1..MODIFIERLIST.len())).expect("Const list, string should return.").to_string();
+        return MODIFIERLIST.get(rnd.gen_range(0..MODIFIERLIST.len())).expect("Const list, string should return.").to_string();
+    }
+    else if s == "description" {
+        return DESCRIPTIONLIST.get(rnd.gen_range(0..DESCRIPTIONLIST.len())).expect("Const list, string should return.").to_string();
     }
     else {
         return String::from("Invalid argument");
